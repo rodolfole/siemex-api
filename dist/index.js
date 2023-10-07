ab2fd3440c8b8783ee88a008365cdcd428ecf485
rodolfole <rodolfolarae@gmail.com>
2023-10-07 17:59:06 -0600

    remove dotenv conf

--- dist/index.js
+++ dist/index.js
@@ -1,21 +1,48 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator["throw"](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+  };
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
-require('dotenv').config();
+// require('dotenv').config();
 const Db_1 = __importDefault(require("./classes/Db"));
 const App_1 = __importDefault(require("./classes/App"));
-(() => __awaiter(void 0, void 0, void 0, function* () {
+(() =>
+  __awaiter(void 0, void 0, void 0, function* () {
     const app = App_1.default.instance;
     const db = Db_1.default.instance;
     yield db.start();
@@ -40,4 +67,4 @@ const App_1 = __importDefault(require("./classes/App"));
     app.listen( process.env.PORT, () => {
         console.log(`Server On Port: ${ process.env.PORT } in ${ process.env.NODE_ENV } mode`);
     });*/
-}))();
+  }))();
